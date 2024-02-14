f882241967ef787ff20319c32c5885f1a42e17e2
sorrrrry <160016106+sorrrrry@users.noreply.github.com>
2024-02-14 18:48:31 +0800

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("хеллоу ворлд");
+        print("не плохая получилась история..");
+        print("ту ту ту ту");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
